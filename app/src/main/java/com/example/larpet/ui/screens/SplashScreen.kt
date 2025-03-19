73e34c23f0c55126cb8d4a0582e6437f559ad3a7
Fagokin <hetcho_dack@hotmail.com>
2025-03-19 20:41:06 -0300

    fix: remove Text component da SplashScreen

--- app/src/main/java/com/example/larpet/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/example/larpet/ui/screens/SplashScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.larpet.R
@@ -48,13 +47,6 @@ fun SplashScreen(navController: NavHostController) {
                 modifier = Modifier
                     .size(200.dp)
                     .scale(scale)
-            )
-            
-            Spacer(modifier = Modifier.height(16.dp))
-            
-            Text(
-                text = stringResource(R.string.app_name),
-                style = MaterialTheme.typography.headlineMedium
             )
         }
     }
